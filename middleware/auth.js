cb063e7a37329a9f4a778f44f9b101bc88e33ea9
Naman Pande <62431605+pandenaman007@users.noreply.github.com>
2024-10-11 00:58:11 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+exports.auth = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        req.adminId = decoded.id; // Set the decoded admin ID for further use
+        next();
+    });
+};
